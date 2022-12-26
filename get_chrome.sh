d00163eef99ab4e74e65a86c0308088171fda4dd
shizz <steeji.rat@gmil.com>
2022-12-26 19:06:25 +0300

    added dockerfile for container usage in ci\cd

---
+++ get_chrome.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+cd $(dirname $0)
+
+LASTCHANGE_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2FLAST_CHANGE?alt=media"
+
+REVISION=$(curl -s -S $LASTCHANGE_URL)
+
+echo "latest revision is $REVISION"
+
+if [ -d $REVISION ] ; then
+  echo "already have latest version"
+  exit
+fi
+
+ZIP_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F$REVISION%2Fchrome-linux.zip?alt=media"
+
+ZIP_FILE="${REVISION}-chrome-linux.zip"
+
+echo "fetching $ZIP_URL"
+
+rm -rf $REVISION
+mkdir $REVISION
+pushd $REVISION
+curl -# $ZIP_URL > $ZIP_FILE
+echo "unzipping.."
+unzip $ZIP_FILE
+popd
+rm -f ./latest
+ln -s $REVISION/chrome-linux/ ./latest
